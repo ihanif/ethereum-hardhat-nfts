34b55523fba1661c6233d452a28d6e50edc3a824
Hanif U Rahman <hanif@Hanifs-MacBook-Air.local>
2021-12-15 23:01:56 +0000

    feat: Update contract to mint NFTs
    - Add code to MyEpicContract to mint NFTs using openzeplin standard
    - Add deploy script to use for deployments
    - Update config to use Alchemy and rinkeyby network
    - Deploy contract to rinkeby testnet

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,10 +2,35 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+contract MyEpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquarNFT", "SQUARE") {
         console.log("Hello, Hardhat NFT Contract!");
+    }
+
+    // A function our user will hit to get their NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/UBYD");
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
     }
 }
